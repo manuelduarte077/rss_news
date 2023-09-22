b8199191007964e7f8f8e7221e82cb973755ca6e
Manuel Duarte  <carlosd99m@gmail.com>
2023-09-21 23:48:22 -0600

    create database controller

---
+++ lib/features/add_rss/database/database_controller.dart
@@ -0,0 +1,58 @@
+import 'package:rss_news/features/add_rss/database/todo_database.dart';
+import 'package:rss_news/features/add_rss/model/rss_model.dart';
+
+class DatabaseController {
+  final dbClient = DatabaseProvider.dbProvider;
+
+  Future<int> saveRSS(RssModel rss) async {
+    // the code below is used to get the access to the db getter
+    final db = await dbClient.db;
+
+    final result = db.insert('Rss', rss.toJson());
+
+    return result;
+  }
+
+  Future<List<RssModel>> getAllRss({List<String>? columns}) async {
+    // the code below is used to get the access to the db getter
+    final db = await dbClient.db;
+
+    // the code below is used to query the database
+    final result = await db.query('Rss', columns: columns);
+
+    List<RssModel> rss = result.isNotEmpty
+        ? result.map((rss) => RssModel.fromJson(rss)).toList()
+        : [];
+
+    return rss;
+  }
+
+  // the code below is used to create a method to update the todoTable
+  Future<int> updateRss(RssModel rss) async {
+    // the code below is used to get the access to the db getter
+    final db = await dbClient.db;
+
+    final result = await db.update(
+      'Rss',
+      rss.toJson(),
+      where: 'id = ?',
+      whereArgs: [rss.id],
+    );
+
+    return result;
+  }
+
+  //the method below is used to Delete Todo records
+  Future<int> deleteRss(int id) async {
+    // the code below is used to get the access to the db getter
+    final db = await dbClient.db;
+
+    final result = await db.delete(
+      'Rss',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    return result;
+  }
+}
---
+++ lib/features/add_rss/database/todo_database.dart
@@ -0,0 +1,55 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseProvider {
+  // the code below is used to create a static final instance of the Database provider class
+  static final DatabaseProvider dbProvider = DatabaseProvider();
+
+  // the code below is used to create a static final instance of the database
+  Database? _database;
+
+  // the code below is used to create a getter for checking that if the database
+  // is not null then returning the database else creating a new database
+  Future<Database> get db async {
+    if (_database != null) {
+      return _database!;
+    } else {
+      _database = await createDatabase();
+      return _database!;
+    }
+  }
+
+  // the code below is used to create a method to create the database
+  Future<Database> createDatabase() async {
+    Directory docDirectory = await getApplicationDocumentsDirectory();
+
+    String path = join(docDirectory.path, 'todo.db');
+
+    await deleteDatabase(path);
+
+    final database = await openDatabase(
+      path,
+      version: 2,
+      onCreate: (db, version) async {
+        await db.execute(tableTodo);
+      },
+      onUpgrade: (db, oldVersion, newVersion) {
+        if (newVersion > oldVersion) {}
+      },
+    );
+
+    return database;
+  }
+
+  static const tableTodo = """
+          CREATE TABLE IF NOT EXISTS Rss (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            link TEXT,
+            description TEXT,
+          );
+      """;
+}
